b870f0b4722a7e1325a64e50d3eeeb20dfb60d07
XetPy1030 <xetpy1030@mail.ru>
2024-03-21 18:28:36 +0300

    Fix env variables

--- src/consts/api.js
+++ src/consts/api.js
@@ -1,4 +1,4 @@
-const HOST = process.env.BACKEND_HOST || "localhost:8000";
+const HOST = process.env.REACT_APP_BACKEND_HOST || "localhost:8000";
 
 export const BASE_URL = `http://${HOST}/api`;
 export const WS_URL = `ws://${HOST}/api`;
--- src/consts/keycloak.js
+++ src/consts/keycloak.js
@@ -1,3 +1,3 @@
-export const KEYCLOAK_URL = process.env.KEYCLOAK_URL || 'https://auth.dev.hr.alabuga.space/auth';
-export const KEYCLOAK_REALM = process.env.KEYCLOAK_REALM || 'Alabuga';
-export const KEYCLOAK_CLIENT_ID = process.env.KEYCLOAK_CLIENT_ID || 'alb-game';
+export const KEYCLOAK_URL = process.env.REACT_APP_KEYCLOAK_URL || 'https://auth.dev.hr.alabuga.space/auth';
+export const KEYCLOAK_REALM = process.env.REACT_APP_KEYCLOAK_REALM || 'Alabuga';
+export const KEYCLOAK_CLIENT_ID = process.env.REACT_APP_KEYCLOAK_CLIENT_ID || 'alb-game';
